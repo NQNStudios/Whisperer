2380ace1ec1404c97df9319fd17d79a3affa91c1
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-05-14 14:26:28 -0600

    Added demo of new ASCIILib music features, as well as mouse input. Click to move the caption.

--- Whisperer/Whisperer/main.cpp
+++ Whisperer/Whisperer/main.cpp
@@ -11,31 +11,64 @@ const int kWindowHeight = 40;
 
 ascii::Surface* gSurface;
 
-void LoadContent(ascii::ImageCache* cache)
+int gSurfaceX = 0;
+int gSurfaceY = 0;
+
+bool gDrawn = false;
+
+void LoadContent(ascii::ImageCache* cache, ascii::SoundManager* soundManager)
 {
-	gSurface = ascii::Surface::FromFile("MySurface.srf");
+	gSurface = ascii::Surface::FromFile("Data/caption.srf");
 	
-	cache->loadTexture("forest", "Untitled artwork.png", ascii::Color::Maroon);
+	cache->loadTexture("forest", "Data/Untitled artwork.png", ascii::Color::Maroon);
+
+	soundManager->addSound("fireball", "Data/fireball.wav");
+	soundManager->addSound("splash", "Data/sunsplash.wav");
+	soundManager->addTrack("space", "Data/Space Fighter Loop.mp3");
 }
 
 void Update(ascii::Game* game, int deltaMS)
 {
 	std::cout << "FPS: " << 1.0f / ((float)deltaMS / 1000.0f);
+
+	if (!gDrawn)
+	{
+		gDrawn = true;
+
+		game->graphics()->clearTransparent();
+		game->graphics()->blitSurface(gSurface, gSurfaceX, gSurfaceY);
+		game->graphics()->addBackgroundImage("forest", "forest", 0, 0);
+	
+		game->graphics()->update();
+	}
 }
 
 void HandleInput(ascii::Game* game, ascii::Input& input)
 {
-	
+	if (input.wasKeyPressed(SDLK_SPACE))
+	{
+		if (game->soundManager()->trackPlaying())
+		{
+			game->soundManager()->fadeOutTrack(2000);
+		}
+		else
+		{
+			game->soundManager()->fadeInTrack("space", -1, 2000);
+		}
+	}
+
+	if (input.mouseButtonDown(ascii::LEFT))
+	{
+		gSurfaceX = input.mouseX() / game->graphics()->charWidth();
+		gSurfaceY = input.mouseY() / game->graphics()->charHeight();
+
+		gDrawn = false;
+	}
 }
 
-void Draw(ascii::Graphics& graphics)
+void Draw(ascii::Game* game)
 {
-	graphics.clear();
-
-	graphics.blitSurface(gSurface, 0, 0);
-	//graphics.addBackgroundImage("forest", "forest", 0, 0);
 	
-	graphics.update();
 }
 
 int main(int argc, char** argv)
