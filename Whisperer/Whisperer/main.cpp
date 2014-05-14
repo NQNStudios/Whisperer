4a4e475ca512c02929f59973c9bff431f0f39f31
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-05-13 21:33:10 -0600

    Added more SDL extension libraries.

--- Whisperer/Whisperer/main.cpp
+++ Whisperer/Whisperer/main.cpp
@@ -1,5 +1,9 @@
 #include "Game.h"
 
+#include <iostream>
+
+#include <SDL_mixer.h>
+
 const char* kWindowTitle = "The Whisperer in Darkness";
 
 const int kWindowWidth = 100;
@@ -9,12 +13,14 @@ ascii::Surface* gSurface;
 
 void LoadContent(ascii::ImageCache* cache)
 {
-	gSurface = ascii::Surface::FromFile("MySurface.srf", cache);
+	gSurface = ascii::Surface::FromFile("MySurface.srf");
+	
+	cache->loadTexture("forest", "Untitled artwork.png", ascii::Color::Maroon);
 }
 
 void Update(ascii::Game* game, int deltaMS)
 {
-	
+	std::cout << "FPS: " << 1.0f / ((float)deltaMS / 1000.0f);
 }
 
 void HandleInput(ascii::Game* game, ascii::Input& input)
@@ -27,7 +33,8 @@ void Draw(ascii::Graphics& graphics)
 	graphics.clear();
 
 	graphics.blitSurface(gSurface, 0, 0);
-
+	//graphics.addBackgroundImage("forest", "forest", 0, 0);
+	
 	graphics.update();
 }
 
